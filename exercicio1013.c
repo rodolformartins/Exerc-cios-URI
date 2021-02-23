80c2b3363fe00cefad42b62bf6a1b0c36179367b
Rodolfo R. Martins <rodolformartins@outlook.com>
2021-02-23 19:35:10 -0300

    add comentario

--- exercicio1013.c
+++ exercicio1013.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+Entrada
+
+O arquivo de entrada contém três valores inteiros.
+Saída
+
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1013
+ */
+
+
 int main(void){
     int a, b, c, maior;
 
